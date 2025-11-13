023a2ecd35b253392ab02328423924c54b0c8d21
Kayode Oguntimehin <jkayogunz@gmail.com>
2025-11-13 14:31:50 +0100

    add gradle build files and setup

--- src/main/java/kv/MoniepointKeyValueStoreApplication.java
+++ src/main/java/kv/MoniepointKeyValueStoreApplication.java
@@ -1,6 +1,6 @@
 package kv;
 
-public class Main {
+public class MoniepointKeyValueStoreApplication {
     public static void main(String[] args) {
         System.out.println("Moniepoint Key Value Storage Server starting...");
     }
